d2c942b3b5a32ff852d26ead2415fa5d1dc497f3
Cicero Viana <cicerohen@gmail.com>
2023-05-24 09:24:16 -0300

    chore: configures mfe deploy script.

--- scripts/deploy-mfe.js
+++ scripts/deploy-mfe.js
@@ -2,51 +2,77 @@
 
 const path = require("path");
 const fs = require("fs");
-
 const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
+const aws4 = require("aws4");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
+
 require("dotenv").config({
-  path: path.resolve(__dirname, "..", ".env"),
+  path: path.resolve(__dirname, "..", `.env.${process.env.NODE_ENV}`),
 });
 
-const projectName = path.basename(process.cwd());
-const s3 = new S3Client({ region: "us-east-1" });
+const mfePackageJson = JSON.parse(
+  fs.readFileSync(path.resolve(process.cwd(), "./package.json"), "utf8")
+);
 
-const uploadToS3 = async () => {
+const MFE_FOLDER_NAME = path.basename(process.cwd());
+const MFE_DIST_FOLDER_PATH = path.resolve(process.cwd(), "./dist");
+const BUCKET_NAME = `typer-app-mfe-${process.env.STAGE}`;
+const SERVICE_NAME = mfePackageJson.name;
+
+const s3 = new S3Client({ region: process.env.REGION });
+
+const uploadFiles = async () => {
   try {
-    const distFolderPath = path.resolve(process.cwd(), "./dist");
-    const files = fs.readdirSync(distFolderPath);
+    const files = fs.readdirSync(MFE_DIST_FOLDER_PATH);
 
     await Promise.all(
       files.map(async (file) => {
-        const filePath = `${distFolderPath}/${file}`;
+        const filePath = path.join(MFE_DIST_FOLDER_PATH, file);
         const fileStream = fs.createReadStream(filePath);
 
-        const params = {
-          Bucket: bucketName,
-          Key: `${destinationPrefix}/${file}`,
-          Body: fileStream,
-          CacheControl: cacheControl,
-        };
-
-        await s3.upload(params).promise();
-        console.log(`Uploaded file: ${file}`);
+        await s3.send(
+          new PutObjectCommand({
+            Bucket: BUCKET_NAME,
+            Key: `${MFE_FOLDER_NAME}/${file}`,
+            Body: fileStream,
+          })
+        );
       })
     );
+
+    if (MFE_FOLDER_NAME === "root-config") {
+      const filePath = path.join(MFE_DIST_FOLDER_PATH, "index.html");
+      const fileStream = fs.createReadStream(filePath);
+      await s3.send(
+        new PutObjectCommand({
+          Bucket: BUCKET_NAME,
+          Key: "index.html",
+          Body: fileStream,
+        })
+      );
+    }
   } catch (err) {
     console.log(err);
   }
 };
 
-uploadToS3();
+// const signedRequest = aws4.sign(
+//   {
+//     service: "execute-api",
+//     method: "PATCH",
+//     host: process.env.DEPLOYER_API_GATEWAY_HOST,
+//     path: "/services",
+//     body: "{test: {}}",
+//   },
+//   {
+//     secretAccessKey: process.env.AWS_ACCESS_KEY_ID,
+//     accessKeyId: process.env.AWS_SECRET_ACCESS_KEY,
+//   }
+// );
 
-// # echo "Sync ${PROJECT_NAME} assets to S3"
-// # aws s3 sync dist/ s3://$BUCKET_NAME/$PROJECT_NAME --cache-control max-age=31536000
-
-// # if [ $PROJECT_NAME == "mfe-root-config" ]; then
-// #     echo "Sync index.html to bucket root folder"
-// #     aws s3 cp dist/index.html s3://$BUCKET_NAME/index.html
-// # fi
+// uploadFiles();
 
 // # echo "Update importmap with ${PROJECT_NAME} URL"
 // # curl -u $DEPLOYER_USERNAME:$DEPLOYER_PASSWORD -d '{ "service": "@mfe/'$SERVICE_NAME'" ,"url": "'$PUBLIC_URL'/'$PROJECT_NAME'/'$PROJECT_NAME'.js" }' -X PATCH $DEPLOYER_URL/services\?env=$DEPLOYER_ENV -H "Accept:application/json" -H "Content-Type:application/json"
